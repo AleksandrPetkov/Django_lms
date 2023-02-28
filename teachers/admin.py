864637f064a8c423e6153672131f228154fda19c
Oleksandr Petkov <sasha111108@gmail.com>
2023-02-28 09:04:35 +0200

    homework

--- teachers/admin.py
+++ teachers/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Teacher
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'salary')
+    list_display_links = ('first_name', 'last_name', 'salary')
+    list_per_page = 17
+    search_fields = ('first_name', 'last_name')
+
+    def get_age(self, instance):
+        return instance.get_age()
+
+    readonly_fields = ('get_age',)
+
+    get_age.short_description = 'Age'
+
+    fieldsets = (
+        ('Personal info', {'fields': (('first_name', 'last_name'),)}),
+        ('Born', {'fields': (('birthday', 'get_age'),)}),
+        ('Contact info', {'fields': (('email', 'city'),)}),
+        ('Salary', {'fields': ('salary',)})
+    )
+
+
+
